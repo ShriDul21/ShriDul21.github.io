4e0b4ff9da10eb1ef27bc8b66d7da6874be6726b
Shrikar Dulam <shrikar.dulam@gmail.com>
2025-06-18 00:39:25 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <p className="text-red-400 font-bold mt-10">Tailwind is working.</p>
+      {/* <p className="text-red-400 font-bold mt-10">Tailwind is working.</p> */}
     </>
   )
 }
