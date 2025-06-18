d1b0bd3e98ff5769aa444904be7a4d852813e0cf
Shrikar Dulam <shrikar.dulam@gmail.com>
2025-06-18 00:27:53 -0500

    debuggin

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/ShriDul21.github.io', // repo name
+  base: '/shridul21.github.io', // repo name
 })
